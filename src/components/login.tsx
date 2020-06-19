2b8be991a0c159112d65516a5c58052dc0315e94
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2020-06-19 02:43:41 +0200

    basic login form

---
+++ src/components/login.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+
+interface IState {
+    name: string,
+    host: string,
+    port: string,
+    callback: (name: string, host: string, port: number) => void
+}
+
+interface IProp {
+    callback: (name: string, host: string, port: number) => void
+}
+
+export class LoginForm extends React.Component<IProp, IState> {
+    constructor(props: IProp) {
+        super(props);
+        this.state = {
+            name: '',
+            host: '',
+            port: '',
+            callback: props.callback
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event: React.FormEvent<HTMLInputElement>) {
+        if (event.currentTarget.name == "name") {
+            this.setState({ name: event.currentTarget.value });
+        }
+        else if (event.currentTarget.name == "host") {
+            this.setState({ host: event.currentTarget.value });
+        }
+        else if (event.currentTarget.name == "port") {
+            this.setState({ port: event.currentTarget.value });
+        }
+    }
+
+    handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        const port = parseInt(this.state.port);
+        if (this.state.name != '' && this.state.host != '' && port.toString() != "NaN" && port > 0) {
+            console.log("yeet", this.state);
+            this.state.callback(this.state.name, this.state.host, port);
+        }
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form id="loginForm" className="center" onSubmit={this.handleSubmit}>
+                <input required name="name" type="text" placeholder="Name" value={this.state.name} onChange={this.handleChange}></input>
+                <input required name="host" type="text" placeholder="Hostname" value={this.state.host} onChange={this.handleChange}></input>
+                <input required name="port" type="number" placeholder="Port" value={this.state.port} onChange={this.handleChange}></input>
+
+                <button >Join Game</button>
+            </form>
+        )
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,48 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
+import { LoginForm } from "./components/login";
+
+interface IState {
+    atLogin: boolean,
+    name: string | null,
+    urlBase: string | null
+}
+
+class UI extends React.Component<{}, IState> {
+    
+    constructor(props: {}) {
+        super(props);
+        
+        this.state = {
+            atLogin: true,
+            name: null,
+            urlBase: null
+        };
+
+        this.loginSubmitCallback = this.loginSubmitCallback.bind(this);
+    }
+
+    loginSubmitCallback(name: string, host: string, port: number) {
+        this.setState({
+            name: name,
+            urlBase: "ws://" + host + ":" + port + "/",
+            atLogin: false
+        })
+    }
+
+    render() {
+        if (this.state.atLogin) {
+            return <LoginForm callback={this.loginSubmitCallback} />
+        }
+        else {
+        return <p>Hi, {this.state.name}</p>
+        }
+    }
+
+}
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    <UI />,
+    document.getElementById("login")
 );
